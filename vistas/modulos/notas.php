9ce9d95c22544ab7a757752c2e0ff90324396c88
user.name like : "Anthony Mendoza <uu>
2020-08-07 20:19:58 -0500

    Cambios

---
+++ vistas/modulos/notas.php
@@ -0,0 +1,59 @@
+<div class="notas">
+      <div class="nota">
+        <h3>Lorem ipsum dolor sit </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
+          
+        </p>
+        <a href="#" target="_blank">Leer mas<i class="fas fa-angle-double-right"></i></a>
+      </div>
+
+      <div class="nota">
+        <h3>Lorem ipsum dolor sit </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
+        </p>
+        <a href="#" target="_blank">Leer mas<i class="fas fa-angle-double-right"></i></a>
+      </div>
+      <div class="nota">
+        <h3>Lorem ipsum dolor sit </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
+        </p>
+        <a href="#" target="_blank">Leer mas<i class="fas fa-angle-double-right"></i></a>
+      </div>
+      <div class="nota">
+        <h3>Lorem ipsum dolor sit </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
+        </p>
+        <a href="#" target="_blank">Leer mas<i class="fas fa-angle-double-right"></i></a>
+      </div>
+</div>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <a href="#" class="float" target="_blank">
+    <i id="add" class="fas fa-plus"></i></a>
--- vistas/modulos/tareas.php
+++ vistas/modulos/tareas.php
@@ -20,4 +20,7 @@
         <label class="fecha">| 12:20</label>
         <a href="#" target="_blank">Editar<i class="fas fa-angle-double-right"></i></a>
       </div>
-    </div>
+</div>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<a href="#" class="float" target="_blank">
+<i id="add" class="fas fa-plus"></i></a>
