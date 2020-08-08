be5008fceeb686af38e0244cd605e9d1f587dc1e
AngeloOZ <53668978+AngeloOZ@users.noreply.github.com>
2020-08-07 23:26:41 -0500

    img

--- vistas/modulos/mod.php
+++ vistas/modulos/mod.php
@@ -7,23 +7,23 @@
             <div class="contenedor_opciones">
                 <div class="form-group-radios">
                     <input type="radio" name="estadoAnimo" value="Muy Bien" id="estadoMuyBien">
-                    <label class="label" for="estadoMuyBien"><span>Muy Bien</span><img src="./img/Emoji svg/1 muyBueno (2).svg" alt=""></label>
+                    <label class="label" for="estadoMuyBien"><span>Muy Bien</span><img src="vistas/img/Emoji svg/1 muyBueno (2).svg" alt=""></label>
                 </div>
                 <div class="form-group-radios">
                     <input type="radio" name="estadoAnimo" value="Bien" id="estadoBien">
-                    <label class="label" for="estadoBien"><span>Bien</span><img src="./img/Emoji svg/2 bueno (2).svg" alt=""></label>
+                    <label class="label" for="estadoBien"><span>Bien</span><img src="vistas/img/Emoji svg/2 bueno (2).svg" alt=""></label>
                 </div>
                 <div class="form-group-radios">
                     <input type="radio" name="estadoAnimo" value="Regular" id="estadoRegular">
-                    <label class="label" for="estadoRegular"><span>Regular</span><img src="./img/Emoji svg/3 regular (2).svg" alt=""></label>
+                    <label class="label" for="estadoRegular"><span>Regular</span><img src="vistas/img/Emoji svg/3 regular (2).svg" alt=""></label>
                 </div>
                 <div class="form-group-radios">
                     <input type="radio" name="estadoAnimo" value="Malo" id="estadoMalo">
-                    <label class="label" for="estadoMalo"><span>Malo</span><img src="./img/Emoji svg/4 malo (2).svg" alt=""></label>
+                    <label class="label" for="estadoMalo"><span>Malo</span><img src="vistas/img/Emoji svg/4 malo (2).svg" alt=""></label>
                 </div>
                 <div class="form-group-radios">
                     <input type="radio" name="estadoAnimo" value="Muy Malo" id="estadoMuyMalo">
-                    <label class="label" for="estadoMuyMalo"><span>Muy Malo</span><img src="./img/Emoji svg/5 muyMaloV2 (2).svg" alt=""></label>
+                    <label class="label" for="estadoMuyMalo"><span>Muy Malo</span><img src="vistas/img/Emoji svg/5 muyMaloV2 (2).svg" alt=""></label>
                 </div>
             </div>
             <?php 
