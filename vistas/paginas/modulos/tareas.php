0c09b98a4508340e75b76db89ab389beea6311f8
user.name like : "Anthony Mendoza <uu>
2020-08-09 14:57:41 -0500

    cambios

--- vistas/paginas/modulos/tareas.php
+++ vistas/paginas/modulos/tareas.php
@@ -1,22 +1,22 @@
 <head>
 <link rel="stylesheet" type="text/css" href="./vistas/css/tareas.css">
 </head>
-<div class="tasks">
-      <div class="task">
+<div class="tareas">
+      <div class="tarea">
         <h3 class="th3">Lorem ipsum dolor sit </h3>
         <i id="clock" class="far fa-clock"></i>
         <label class="fecha">2020-12-12</label>
         <label class="fecha">| 12:20</label>
         <a href="#" target="_blank">Editar<i class="fas fa-angle-double-right"></i></a>
       </div>
-      <div class="task">
+      <div class="tarea">
         <h3 class="th3">Lorem ipsum dolor sit </h3>
         <i id="clock" class="far fa-clock"></i>
         <label class="fecha">2020-12-12</label>
         <label class="fecha">| 12:20</label>
         <a href="#" target="_blank">Editar<i class="fas fa-angle-double-right"></i></a>
       </div>
-      <div class="task">
+      <div class="tarea">
         <h3 class="th3">Lorem ipsum dolor sit </h3>
         <i id="clock" class="far fa-clock"></i>
         <label class="fecha">2020-12-12</label>
