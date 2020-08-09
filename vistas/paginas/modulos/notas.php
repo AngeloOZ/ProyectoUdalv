a0b4dbf035e03e8dc9e6b901eb47a2e0d389480d
AngeloOZ <53668978+AngeloOZ@users.noreply.github.com>
2020-08-09 17:24:08 -0500

    cambios

--- vistas/paginas/modulos/notas.php
+++ vistas/paginas/modulos/notas.php
@@ -1,4 +1,4 @@
-<div class="note-box">
+<!-- <div class="note-box">
       <form action="#" method="post">
         <input type="text" placeholder="Nombre" name="nombre">
         <textarea class="descripcion" name="descripcion" rows="8" cols="71" placeholder="Descripcion"></textarea>
@@ -8,7 +8,7 @@
           $agregar->ctrAgregarNotas();
         ?>
       </form>
-</div>  
+</div>   -->
 <!-- <a href="#" class="float" target="_blank"> -->
 <!-- <i id="add" class="fas fa-undo-alt"></i>  -->
 <div class="notas">
@@ -19,3 +19,9 @@
 </div>
 <a href="calendario" class="float" target="_blank">
 <i id="add" class="fas fa-plus"></i></a>
+
+
+<form method="post">
+  <input type="hidden" name="" value="id">
+  <input type="submit" value="">
+</form>
