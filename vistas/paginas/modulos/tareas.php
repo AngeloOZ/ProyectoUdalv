5ee12429c6fda9cbefcea39f1f85fc3092e7e1af
user.name like : "Anthony Mendoza <uu>
2020-08-09 14:51:27 -0500

    cambios

--- vistas/paginas/modulos/tareas.php
+++ vistas/paginas/modulos/tareas.php
@@ -1,20 +1,20 @@
-<div class="notas">
-      <div class="nota">
-        <h3>Lorem ipsum dolor sit </h3>
+<div class="tasks">
+      <div class="task">
+        <h3 class="th3">Lorem ipsum dolor sit </h3>
         <i id="clock" class="far fa-clock"></i>
         <label class="fecha">2020-12-12</label>
         <label class="fecha">| 12:20</label>
         <a href="#" target="_blank">Editar<i class="fas fa-angle-double-right"></i></a>
       </div>
-      <div class="nota">
-        <h3>Lorem ipsum dolor sit </h3>
+      <div class="task">
+        <h3 class="th3">Lorem ipsum dolor sit </h3>
         <i id="clock" class="far fa-clock"></i>
         <label class="fecha">2020-12-12</label>
         <label class="fecha">| 12:20</label>
         <a href="#" target="_blank">Editar<i class="fas fa-angle-double-right"></i></a>
       </div>
-      <div class="nota">
-        <h3>Lorem ipsum dolor sit </h3>
+      <div class="task">
+        <h3 class="th3">Lorem ipsum dolor sit </h3>
         <i id="clock" class="far fa-clock"></i>
         <label class="fecha">2020-12-12</label>
         <label class="fecha">| 12:20</label>
