2e05a330361e68fdd46ec5438da1aea3e744a543
user.name like : "Anthony Mendoza <uu>
2020-08-09 15:20:45 -0500

    cambios

--- vistas/paginas/modulos/notas.php
+++ vistas/paginas/modulos/notas.php
@@ -55,5 +55,5 @@
       </div>
 </div>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<a href="añadir_nota.php" class="float" target="_blank">
+<a href="./ProyectoUdalv/vistas/paginas/modulos/añadir_nota.php" class="float" target="_blank">
 <i id="add" class="fas fa-plus"></i></a>
